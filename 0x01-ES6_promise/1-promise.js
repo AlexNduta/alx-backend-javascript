f2a2af9085c7c5921c60809fa46ccec5e28c1e6f
was <was@localhost.localdomain>
2024-03-07 09:43:13 +0300

    still

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,15 @@
-export default function getFullResponseFromAPI(success) {
-  if (success) return Promise.resolve({ status: 200, body: 'Success' });
-  return Promise.reject(new Error('The fake API is not working currently'));
+export default function getFullResponseFromAPI(success){
+    let promise = new Promise((resolve, reject)=>{
+        if (success){
+            return resolve({
+            status:200,
+            body:'Success'
+        })
+        } else {
+            return reject('The fake API is not working currently')
+        }
+    })
+
+promise.then(message =>{console.log(message)}, (error)=>{console.log(error)})
 }
+
