37b9fde7c2a1815944159299c1265aff4c6ff170
was <was@localhost.localdomain>
2024-03-12 22:42:28 +0300

    lint

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
 import getListStudents from './getListStudents.js';
 
-export default function getStudentIdsSum(array){
-    const studentIds = array.reduce((acumulator, item) =>item.id + acumulator, 0);
+export default function getStudentIdsSum(array) {
+  const studentIds = array.reduce((acumulator, item) => item.id + acumulator, 0);
 
-    return studentIds;
+  return studentIds;
 }
