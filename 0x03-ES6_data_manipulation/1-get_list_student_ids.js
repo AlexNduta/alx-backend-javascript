16ec613d15a25b787fcf56d6c89fc5d6bc960831
was <was@localhost.localdomain>
2024-03-12 19:20:48 +0300

    wrking

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(arr){
+    if (arr instanceof Object){
+        const resultId = arr.map(item => (item.id));
+        return resultId;
+    } else {
+        return [];
+    }
+}
