a1c4ce7d367feebbb5554710ff1640e3bac33d27
was <was@localhost.localdomain>
2024-03-12 16:30:39 +0300

    fixing

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,7 @@
 export default function getListStudents() {
-  const array = [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }];
+    return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', l  ocation: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },];
   return array;
 }
