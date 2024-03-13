cba3bd7bc3a1a9f864063d39d4d6984b7fe4c9bc
was <was@localhost.localdomain>
2024-03-13 09:02:04 +0300

    task

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
-import getListStudents from './getListStudents.js';
-
 export default function getStudentIdsSum(array) {
   const studentIds = array.reduce((acumulator, item) => item.id + acumulator, 0);
 
