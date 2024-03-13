060de0d6b46c1f1d9e78cd87afa3c5304d666eb8
was <was@localhost.localdomain>
2024-03-13 08:59:59 +0300

    lets

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
-import from './0-get_list_students.js';
-export default function getStudentsByLocation(array, city){
-    const extractedCities = array.filter((item) => item.location === city);
-    return extractedCities;
+// import from "./0-get_list_students.js";
+export default function getStudentsByLocation(array, city) {
+  const extractedCities = array.filter((item) => item.location === city);
+  return extractedCities;
 }
