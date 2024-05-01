4996fb118c32fb889448d04fac4f3dffdbd09294
was <was@localhost.localdomain>
2024-05-01 04:16:12 +0300

    updating

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,16 @@
-const readline = require('readline');
-
+const readline = require('readline')
 const reader = readline.createInterface({
-  input: process.stdin,
-  ouput: process.stdout,
+    input: process.stdin,
+    ouput: process.stdout
 });
 
-console.log('Welcome to Holberton School, what is your name?\n');
-reader.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  reader.close();
+console.log("Welcome to Holberton School, what is your name?");
+reader.question( "Welcome to Holberton School, what is your name?", function(name){
+    console.log(`Your name is: ${name}`);
+    reader.close();
 });
 
-reader.on('close', () => {
-  console.log('This important software is now closing');
-  process.exit(0);
+reader.on('close', function(){
+    console.log("This important software is now closing");
+    //process.exit(0);
 });
